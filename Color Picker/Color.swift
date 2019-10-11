72597937dd9643edb0b349865c05abb9cab673d5
Josh <jdbb7d@mail.missouri.edu>
2019-10-11 18:12:51 -0500

    Finished product

---
+++ Color Picker/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Color Picker
+//
+//  Created by Joshua Brooks on 10/11/19.
+//  Copyright © 2019 Joshua Brooks. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+}
